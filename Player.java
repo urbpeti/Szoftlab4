88d79c75393168f1aca8b56cb25912f42dc685a7
Peter <urbpeti@gmail.com>
2015-02-27 20:07:28 +0100

    At lehet allitani a billentyutket

--- Player.java
+++ Player.java
@@ -2,25 +2,31 @@ import static java.awt.event.KeyEvent.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-
 public class Player implements KeyListener {
 	private Robot robot;
-	
+	private int acceleration = VK_RIGHT;
+
+	private int decelerate = VK_LEFT;
+
 	public Player(Robot robot) {
 		this.robot = robot;
 	}
 
+	public void setAccelerationKey(int KeyCode) {
+		this.acceleration = KeyCode;
+	}
+	
+	public void setDecelerateKey(int KeyCode) {
+		this.decelerate = KeyCode;
+	}
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case(VK_LEFT):
-			robot.decelerate();
-			break;
-		case(VK_RIGHT):
-			robot.accelerate();
-		default:
-			break;
+		if(e.getKeyCode() == decelerate){
+			robot.decelerate();			
 		}
+		if(e.getKeyCode() == acceleration)
+			robot.accelerate();
 	}
 
 	@Override
