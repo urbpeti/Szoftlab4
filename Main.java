263901d0230847042ebd5815762fd2f1d9d2e7ac
Peter <urbpeti@gmail.com>
2015-03-20 19:34:17 +0100

    Main elkezdve, text fájlból olvassa be a Menu-t

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class Main {
 	public static void main(String[] args) {
-		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedReader fr = new BufferedReader(new FileReader(new File("MenuText.txt")));
+			String sor;
+			while((sor = fr.readLine()) != null){
+				System.out.println(sor);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
